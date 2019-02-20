159d3bd9b5c3a6923d4ab357e9aadc2b12c853d8
ksenya96 <k.pilyak96@tut.by>
2019-02-20 19:15:59 +0200

    updated localsearch by kotov, lepin

--- src/LocalSearch.java
+++ src/LocalSearch.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -31,11 +33,22 @@ public class LocalSearch extends Heuristics {
 
         int numberOfSolutions = 10;
         Random random = new Random();
-        byte[][][] x = new byte[numberOfSolutions][m][T];
+        List<List<List<Byte>>> x = new ArrayList<>(numberOfSolutions);
+        for (int i = 0; i < numberOfSolutions; i++) {
+            x.add(new ArrayList<>(m));
+            for (int j = 0; j < m; j++) {
+                x.get(i).add(new ArrayList<>(T));
+                for (int k = 0; k < T; k++) {
+                    x.get(i).get(j).add((byte)0);
+                }
+            }
+        }
+
         Map<Integer, ArrayList<Integer>> passedSlots = new HashMap<>();
         for (int i = 0; i < m; i++) {
             passedSlots.put(i, new ArrayList<>());
         }
+        //формирование списка допустимых интервалов для каждой операции
         for (int i = 0; i < T; i++) {
             for (int k = 0; k < m; k++) {
                 if (r[getGroupByIndex(k, patientsAndGroups)] + p[k] <= B[i] &&
@@ -45,64 +58,156 @@ public class LocalSearch extends Heuristics {
             }
         }
 
+        //генерация случайных решений
         for (int cnt = 0; cnt < numberOfSolutions; cnt++) {
             for (int i = 0; i < m; i++) {
-                int slot = random.nextInt(passedSlots.get(i).size());
-                x[cnt][i][slot] = (byte)random.nextInt(2);
+                int slot = random.nextInt(T);
+                x.get(cnt).get(i).set(slot, (byte)random.nextInt(2));
             }
         }
 
-        double[] fitnessFunctionValues = new double[numberOfSolutions];
-        A: for (int cnt = 0; cnt < numberOfSolutions; cnt++) {
-            int[] z = new int[T];
-            for (int i = 0; i < T; i++) {
-                for (int k = 0; k < m; k++) {
-                    if (r[getGroupByIndex(k, patientsAndGroups)] + p[k] <= B[i] &&
-                        d[getGroupByIndex(k, patientsAndGroups)] - p[k] >= A[i]) {
-                        int duration = 0;
-                        for (int l = k; l < m; l++) {
-                            duration += p[l] * x[cnt][l][i];
-                            int slotResidue = Math.min(B[i], d[getGroupByIndex(l, patientsAndGroups)]) -
-                                              Math.max(A[i], r[getGroupByIndex(k, patientsAndGroups)]) - duration;
-                            if (slotResidue < 0) {
-                                if (-slotResidue < delta[i]) {
-                                    z[i] = Math.max(z[i], -slotResidue);
-                                }
-                                else {
-                                    fitnessFunctionValues[cnt] = 0;
-                                    continue A;
-                                }
+        //вычисление функции приспособленности
+        double[] fitnessFunctionValues = new double[numberOfSolutions + 1];
+        for (int cnt = 0; cnt < numberOfSolutions; cnt++) {
+            fitnessFunctionValues[cnt] =
+                    calculateFitnessFunction(T, delta, r, A, B, c, m, patientsAndGroups, d, w, p, x.get(cnt));
+        }
+
+        /*for (int i = 0; i < numberOfSolutions; i++) {
+            int patients = 0;
+            if (fitnessFunctionValues[i] > 0.0) {
+                for (int j = 0; j < m; j++) {
+                    for (int k = 0; k < T; k++) {
+                        patients += x.get(i).get(j).get(k);
+                    }
+                }
+            }
+            System.out.println(fitnessFunctionValues[i] + " " + patients);
+        }*/
+
+        for (int iter = 0; iter < 100; iter++) {
+            //вычисление вероятностей выбора решений из популяции
+            double sumFitnessFunctionValues = Arrays.stream(fitnessFunctionValues).sum() - fitnessFunctionValues[numberOfSolutions];
+            double[] pr = Arrays.stream(fitnessFunctionValues).map((v) -> v / sumFitnessFunctionValues).limit(numberOfSolutions).toArray();
+            int parentSolution1 = getRandomValue(pr);
+            int parentSolution2 = getRandomValue(pr);
+            System.out.println(parentSolution1 + " " + parentSolution2);
+
+            //скрещивание
+            List<List<Byte>> newSolution = crossover(T, m, x, parentSolution1, parentSolution2);
+
+            //мутация
+            double randomParameter = 0.3;
+            mutation(T, m, newSolution, randomParameter);
+            x.add(newSolution);
+            fitnessFunctionValues[numberOfSolutions] = calculateFitnessFunction(T, delta, r, A, B, c, m, patientsAndGroups, d, w, p, newSolution);
+            System.out.println(fitnessFunctionValues[numberOfSolutions]);
+
+            int minIndex = 0;
+            for (int i = 1; i < numberOfSolutions + 1; i++) {
+                if (fitnessFunctionValues[i] < fitnessFunctionValues[minIndex]) {
+                    minIndex = i;
+                }
+            }
+            x.remove(minIndex);
+            fitnessFunctionValues[minIndex] = fitnessFunctionValues[numberOfSolutions];
+            System.out.println(Arrays.stream(fitnessFunctionValues).max().getAsDouble());
+        }
+
+    }
+
+    private static List<List<Byte>> crossover(int T, int m, List<List<List<Byte>>> x, int parentSolution1, int parentSolution2) {
+        List<List<Byte>> newSolution = new ArrayList<>(m);
+        for (int i = 0; i < m; i++) {
+            newSolution.add(new ArrayList<>(T));
+            for (int j = 0; j < T; j++) {
+                newSolution.get(i).add((byte)0);
+            }
+        }
+        Random random = new Random();
+        for (int i = 0; i < m; i++) {
+            int parent1Flag = -1;
+            int parent2Flag = -1;
+            for (int j = 0; j < T; j++){
+                if (x.get(parentSolution1).get(i).get(j) == 1) {
+                    parent1Flag = j;
+                }
+                if (x.get(parentSolution2).get(i).get(j) == 1) {
+                    parent2Flag = j;
+                }
+            }
+            if (random.nextInt(2) == 0) {
+                if (parent1Flag > -1) {
+                    newSolution.get(i).set(parent1Flag, (byte)1);
+                }
+            } else {
+                if (parent2Flag > -1) {
+                    newSolution.get(i).set(parent2Flag, (byte)1);
+                }
+            }
+        }
+        return newSolution;
+    }
+
+    private static void mutation(int T, int m, List<List<Byte>> newSolution, double randomParameter) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < T; j++) {
+                if (newSolution.get(i).get(j) == 1 && getRandomValue(randomParameter, 1 - randomParameter) == 0) {
+                    //инверсия
+                    newSolution.get(i).set(j, (byte)0); //(byte)Math.abs(newSolution[i][j] - 1);
+                }
+            }
+        }
+    }
+
+    private static double calculateFitnessFunction(int T, int[] delta, int[] r, int[] A, int[] B, int[] c, int m,
+                                                   int[] patientsAndGroups, int[] d, double[] w, int[] p, List<List<Byte>> x) {
+        int[] z = new int[T];
+        for (int i = 0; i < T; i++) {
+            for (int k = 0; k < m; k++) {
+                if (r[getGroupByIndex(k, patientsAndGroups)] + p[k] <= B[i] &&
+                    d[getGroupByIndex(k, patientsAndGroups)] - p[k] >= A[i]) {
+                    int duration = 0;
+                    for (int l = k; l < m; l++) {
+                        duration += p[l] * x.get(l).get(i);
+                        int slotResidue = Math.min(B[i], d[getGroupByIndex(l, patientsAndGroups)]) -
+                                          Math.max(A[i], r[getGroupByIndex(k, patientsAndGroups)]) - duration;
+                        if (slotResidue < 0) {
+                            if (-slotResidue < delta[i]) {
+                                z[i] = Math.max(z[i], -slotResidue);
+                            }
+                            else {
+                                return 0;
                             }
                         }
                     }
                 }
             }
-            int cost = 0;
-            for (int i = 0; i < T; i++) {
-                cost += c[i] * z[i];
-            }
-            if (cost <= C) {
-                double value = 0;
-                for (int i = 0; i < m; i++) {
-                    for (int j = 0; j < T; j++) {
-                        value += w[getGroupByIndex(i, patientsAndGroups)] * x[cnt][i][j];
-                    }
-                }
-                fitnessFunctionValues[cnt] = value;
-            } else {
-                fitnessFunctionValues[cnt] = 0;
-            }
         }
-
-        for (int i = 0; i < numberOfSolutions; i++) {
-            int patients = 0;
-            for (int j = 0; j < m; j++) {
-                for (int k = 0; k < T; k++) {
-                    patients += x[i][j][k];
+        int cost = 0;
+        for (int i = 0; i < T; i++) {
+            cost += c[i] * z[i];
+        }
+        if (cost <= C) {
+            double value = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < T; j++) {
+                    value += w[getGroupByIndex(i, patientsAndGroups)] * x.get(i).get(j);
                 }
             }
-            System.out.println(fitnessFunctionValues[i] + " " + patients);
+            return value;
+        } else {
+            return 0;
         }
+    }
 
+    static int getRandomValue(double... pr) {
+        double r = Math.random();
+        int i = 0;
+        double s = pr[0];
+        while (i < pr.length && !(r < s)) {
+            s += pr[++i];
+        }
+        return i;
     }
 }
